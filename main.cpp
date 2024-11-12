6c756d1edb55e4068e504386b106802300287d2a
VonEquinox <VonCrystalOrchid@outlook.com>
2024-11-12 09:15:10 +0800

    Add new feature

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <fstream>
+#include <map>
 #include <optional>
 #include <sstream>
 #include <string>
 #include <string_view>
 #include <print>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+
+class JsonObject
+{
+    public:
+    std::variant<std::monostate, bool, int, double, std::string,std::vector<JsonObject>,std::unordered_map<std::string, JsonObject>> Data;
+};
 
 std::optional<std::string> GetJsonStrData(std::string_view JsonPath)
 {
@@ -17,6 +27,18 @@ std::optional<std::string> GetJsonStrData(std::string_view JsonPath)
     }else{
         std::print("Read Failed");
         return {};
+    }
+}
+
+std::optional<JsonObject> JsonParser(std::string_view JsonPath)
+{
+    std::optional<std::string> JsonStrData = GetJsonStrData(JsonPath);
+    if(!JsonStrData.has_value()){
+        //获取json文件失败
+        return {};
+    }else {
+        //获取json文件成功
+
     }
 }
 
