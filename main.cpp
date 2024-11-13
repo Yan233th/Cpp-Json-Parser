85bc565b9970715b04004bbafac38d8984797a0a
Yan233_ <yan233th@126.com>
2024-11-13 11:33:29 +0800

    modify

--- main.cpp
+++ main.cpp
@@ -1,48 +1,55 @@
 #include <fstream>
 #include <map>
+#include <unordered_map>
 #include <optional>
 #include <sstream>
 #include <string>
 #include <string_view>
-#include <print>
-#include <unordered_map>
+#include <iostream>
 #include <variant>
 #include <vector>
+
+#define endl '\n'
 
 class JsonObject
 {
     public:
-    std::variant<std::monostate, bool, int, double, std::string,std::vector<JsonObject>,std::unordered_map<std::string, JsonObject>> Data;
+    std::variant<std::monostate, bool, int, double, std::string, std::vector<JsonObject>, std::unordered_map<std::string, JsonObject>> Data;
 };
 
-std::optional<std::string> GetJsonStrData(std::string_view JsonPath)
+std::optional<std::string> GetJsonStrData (std::string_view JsonPath)
 {
-    std::ifstream TempFile(JsonPath.data());
+    std::ifstream TempFile (JsonPath.data ());
     std::stringstream TempBuffer{};
-    if (TempFile.is_open()) {
-        TempBuffer << TempFile.rdbuf();
-        TempFile.close();
-        std::print("Read Successful");
-        return TempBuffer.str();
-    }else{
-        std::print("Read Failed");
+    if (TempFile.is_open ())
+    {
+        TempBuffer << TempFile.rdbuf ();
+        TempFile.close ();
+        std::cout << "Read Successful" << endl;
+        return TempBuffer.str ();
+    }
+    else
+    {
+        std::cout << "Read Failed" << endl;
         return {};
     }
 }
 
-std::optional<JsonObject> JsonParser(std::string_view JsonPath)
+JsonObject JsonParser (std::string_view JsonStrData)
 {
+    JsonObject current = JsonParser(JsonStrData);
+    return current;
+}
+
+int main ()
+{
+    std::string JsonPath;
+    std::cin >> JsonPath;
     std::optional<std::string> JsonStrData = GetJsonStrData(JsonPath);
-    if(!JsonStrData.has_value()){
-        //获取json文件失败
-        return {};
-    }else {
-        //获取json文件成功
-
+    if (JsonStrData.has_value())
+    {
+        JsonObject main = JsonParser (JsonStrData.value());
     }
-}
-
-int main()
-{
+    else return 1;
     return 0;
 }
